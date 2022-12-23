307dd703bdd19c25391807294edce6dcd98bd6d1
mladion <mladinoiion99@gmail.com>
2022-12-23 16:01:38 +0200

    generated the chessboard with the location of field

---
+++ ProChess/Client/Components/Chessboard.razor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Client.Components
+{
+	public partial class Chessboard
+	{
+        public readonly string[] HorizontalAxis = { "a", "b", "c", "d", "e", "f", "g", "h" };
+        public readonly string[] VerticalAxis = { "1", "2", "3", "4", "5", "6", "7", "8" };
+    }
+}
