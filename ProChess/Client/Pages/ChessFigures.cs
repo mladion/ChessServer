0679962c3d3a3627d2047d175a6c378d23363a41
Emil Gorda <mr.adrog@gmail.com>
2023-01-05 19:01:37 +0200

    add figures_moves

---
+++ ProChess/Client/Pages/ChessFigures.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Client.Pages
+{
+	public class ChessFigures
+	{
+		public string Name { get; set; }
+		public int Row { get; set; }
+		public int Column { get; set; }
+		public string Unicod { get; set; }
+		public string Moves { get; set; }
+	}
+}
+
